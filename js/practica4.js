a8931811c3d8dabf91bba51f9fde672de003bb16
franjavi8776 <franjavi871976@gmail.com>
2024-09-06 18:49:09 -0400

    first commit

---
+++ js/practica4.js
@@ -0,0 +1,24 @@
+/*1) Programa una función para convertir grados Celsius a Fahrenheit y viceversa, pe. miFuncion(0,"C") devolverá 32°F.*/
+
+//! Respuesta aqui
+
+/*2) Programa una función para convertir números de base binaria a decimal y viceversa, pe. miFuncion(100,2) devolverá 4 base 10.*/
+
+//! Respuesta aqui
+
+/*3) Programa una función que dada una fecha válida determine cuantos años han pasado hasta el día de hoy, pe. miFuncion(new Date(1984,4,23)) devolverá 35 años (en 2020).
+ */
+
+//! Respuesta aqui
+
+/*4) Programa una función totalVentas()  que recorra ambos arreglos (productos y ventas), compara los productos con el detalle de ventas y si el producto coinciden con el producto del detalle muestra la cantidad vendida del producto que aparece en el detalle ventas. En consola muestra la cantidad vendida*/
+
+//! Respuesta aqui (descomenta los arreglos antes de empezar el ejercicio)
+/*const productos = [
+  { producto: "tv", precio: "2500", stock: 5 },
+  { producto: "laptop", precio: "4500", stock: 3 },
+  { producto: "smartphone", precio: "5300", stock: 10 },
+];
+const ventas = [
+  { cliente: "Javier", fecha: "2024-09-16", comprobante: "103", total:4500, usuario: "Jose", detalle: [{producto: "smartphone", precio: "5300", stock:7, cantidad: 3 }]},
+];*/
